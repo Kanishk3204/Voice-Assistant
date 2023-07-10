5e603218b0e3e513f8977f68c96b9230cb861aea
Kanishk3204 <97598953+Kanishk3204@users.noreply.github.com>
2023-07-11 01:25:47 +0530

    Update voice_assistant.py

--- voice_assistant.py
+++ voice_assistant.py
@@ -10,56 +10,103 @@ import subprocess
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice' , voices[1].id)
+engine.setProperty('voice', voices[1].id)
 
 def say(sentence):
     engine.say(sentence)
     engine.runAndWait()
 
+
+def greet():
+    hour = int(datetime.datetime.now().hour)
+    if 0 <= hour < 12:
+        print("Good Morning !")
+        say("Good Morning !")
+    elif 12 <= hour < 16:
+        print("Good afternoon ! ")
+        say("Good afternoon ! ")
+        engine.runAndWait()
+    else:
+        print("Good evening  !")
+        say("Good evening  !")
+    print("I am Your personal voice assistant, what can i do for you?")
+    say("I am Your personal voice assistant, what can i do for you?")
+
 def takecommand():
     r=sr.Recognizer()
     with sr.Microphone() as source:
         print('Listening...')
-        r.pause_threshold =  0.6
+        r.pause_threshold =  0.8
         audio=r.listen(source)
         try:
             print('recognising...')
             query = r.recognize_google(audio,language="en-in")
-            print(f"user said:{query}")
+            print(f"You said:{query}")
             return query.lower()
         except:
             return "Sorry!! Something went wrong"
 
+cond = True
 def action():
-    query=takecommand()
-    sites=[["youtube","https://youtube.com"],["wikipedia","https://wikipedia.com"],["google","https://google.com"]]
-    for site in sites:
-        if f"open {site[0]}" in query.lower():
-            say(f"opening {site[0]}")
-            webbrowser.open(site[1])
-    if 'open music' in query:
-        musicpath= "D:\downloads new\Lord.Huron-The.Night.We.Met.mp3"
-        os.startfile(musicpath)
-    if 'time' in query:
-        time = datetime.datetime.now().strftime('%I:%M:%S %p')
-        hour = datetime.datetime.now().strftime('%I:%M %p')
-        print(f"time is {time}")
-        say(f"time is {hour}")
-    # elif "camera" in query or "take a photo" in query:
-    #     ec.capture(0, "Jarvis Camera ", "img.jpg")
-    if 'camera' in query:
-        os.startfile("microsoft.windows.camera:")
-    if 'calculator' in query:
-        os.startfile("microsoft.windows.calculator:")
-    if 'send message at night to kashish' in query:
-        # say('please enter the contact')
-        # contact = input("Enter the name of the contact: ")
-        # message = input("Enter the message: ")
-        pw.sendwhatmsg('+91 84481 79695','raat ko message through voice assistant',5,30,10000)
-    # if 'send message' in query:
-    #     say('please enter the contact')
-    #     contact = input("Enter the name of the contact: ")
-    #     message = input("Enter the message: ")
-    #     pw.sendwhatmsg(contact, message, datetime.datetime.now().hour, datetime.datetime.now().minute + 1)
+    global cond
+    while True:
+        query = takecommand()
+        sites = [["youtube", "https://youtube.com"], ["facebook", "https://facebook.com"],
+                 ["google", "https://google.com"], ["twitter", "https://twitter.com"],
+                 ["word", "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word"],
+                 ["powerpoint", "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PowerPoint"],
+                 ["brave browser", "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Brave"]]
+        for site in sites:
+            if f"open {site[0]}" in query.lower():
+                say(f"opening {site[0]}")
+                webbrowser.open(site[1])
+        if 'stop' in query:
+            print('ok,I will talk to you later, Goodbye!!!')
+            say('ok,I will talk to you later, Goodbye!!!')
+            cond=False
+            break
+        if "play on youtube" in query:
+            video = query.replace('play on youtube', '')
+            say("playing" + video)
+            pw.playonyt(video)
+        elif "search" in query:
+            what = query.replace('search', '')
+            say(f"searching {what}")
+            pw.search(what)
+        elif 'who is' in query:
+            person = query.replace('who is', '')
+            info = wikipedia.summary(person, 1)
+            print(info)
+            say(info)
+        elif 'what is' in query:
+            person = query.replace('what is', '')
+            info = wikipedia.summary(person, 1)
+            print(info)
+            say(info)
+        elif 'joke' in query:
+            joke = pyjokes.get_joke()
+            print(joke)
+            say(joke)
+        elif 'open music' in query:
+            musicpath = "D:\downloads new\Lord.Huron-The.Night.We.Met.mp3"
+            os.startfile(musicpath)
+        elif 'time' in query:
+            time = datetime.datetime.now().strftime('%I:%M:%S %p')
+            hour = datetime.datetime.now().strftime('%I:%M %p')
+            print(f"time is {time}")
+            say(f"time is {hour}")
+        elif 'camera' in query:
+            os.startfile("microsoft.windows.camera:")
+        elif 'settings' in query:
+            subprocess.Popen('control.exe', shell=True)
+        elif 'send message on whatsapp' in query:
+            say('enter phone number')
+            phone=input('phone number:- ')
+            say('enter message')
+            message=input('message:- ')
+            pw.sendwhatmsg(phone,message,datetime.datetime.now().hour,datetime.datetime.now().minute+1)
 
-action()
+greet()
+
+while cond:
+    action()
